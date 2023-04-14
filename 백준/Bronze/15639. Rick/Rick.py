13bab7730e855efc094e9256eb93072716a87fd9
Jinjae Park <parkjinjae0627@gmail.com>
2023-04-14 21:04:28 +0900

    [Bronze V] Title: Rick, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/15639. Rick/Rick.py
@@ -0,0 +1,15 @@
+rick_wont_do = [
+    "Give you up",
+    "Let you down",
+    "Run around and desert you",
+    "Make you cry",
+    "Say goodbye",
+    "Tell a lie and hurt you"
+]
+
+what_will_do = input()
+
+if what_will_do in rick_wont_do:
+    print("NO")
+else:
+    print("YES")
