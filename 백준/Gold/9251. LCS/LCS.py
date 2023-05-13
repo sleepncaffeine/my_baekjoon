f6c7ce54e1567b16fcefb29ca02103850c492791
Jinjae Park <parkjinjae0627@gmail.com>
2023-05-13 21:24:52 +0900

    [Gold V] Title: LCS, Time: 172 ms, Memory: 122852 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,22 @@
+str1 = input()
+str2 = input()
+
+
+def lcs(str1, str2):
+    m = len(str1)
+    n = len(str2)
+
+    dp = [[0 for i in range(n + 1)] for j in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+            elif str1[i - 1] == str2[j - 1]:
+                dp[i][j] = 1 + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[m][n]
+
+
+print(lcs(str1, str2))
