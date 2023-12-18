9038efebbb3c73730d7481b81eda5f31c25dff0b
Jinjae Park <parkjinjae0627@gmail.com>
2023-12-18 21:50:56 +0900

    [Gold V] Title: 신기한 소수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,29 +1,23 @@
-import sys
-
-sys.setrecursionlimit(10000)
-input = sys.stdin.readline
-
-
-def is_prime(n):
-    if n == 1:
+def isprime(n):
+    if n <= 1:
         return False
-    for i in range(2, int(n ** (1 / 2)) + 1):
-        if not n % i:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
     return True
 
 
-def dfs(n, cnt):
-    if cnt == N:
+def dfs(n, d):
+    if d == N:
         print(n)
         return
     for i in range(1, 10):
-        if is_prime(n * 10 + i):
-            dfs(n * 10 + i, cnt + 1)
+        if isprime(n * 10 + i):
+            dfs(n * 10 + i, d + 1)
 
 
 N = int(input())
 
-for i in range(1, 10):
-    if is_prime(i):
+for i in range(2, 10):
+    if isprime(i):
         dfs(i, 1)
