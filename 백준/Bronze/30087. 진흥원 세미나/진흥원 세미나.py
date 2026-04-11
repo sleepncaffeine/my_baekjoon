effd2f8c00cc04ae9d45e200d6702853ac0c8843
Jinjae Park <parkjinjae0627@gmail.com>
2026-04-11 23:32:38 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,13 @@
+d = {
+    "Algorithm": 204,
+    "DataAnalysis": 207,
+    "ArtificialIntelligence": 302,
+    "CyberSecurity": "B101",
+    "Network": 303,
+    "Startup": 501,
+    "TestStrategy": 105,
+}
+
+t = int(input())
+for _ in range(t):
+    print(d[input()])
